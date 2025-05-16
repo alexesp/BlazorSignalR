99a4e50ba14e4cd9a7b5f5f7f1d5f0be7f82ffc6
alexesp <alexizaes@hotmail.com>
2025-05-16 14:31:38 +0200

    Crear hub de signalr

--- BlazorSignalR/Program.cs
+++ BlazorSignalR/Program.cs
@@ -1,4 +1,5 @@
 using BlazorSignalR.Components;
+using BlazorSignalR.Hubs;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -27,6 +28,8 @@ app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
+app.MapBlazorHub();
+app.MapHub<ChatHub>("/chathub");
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
